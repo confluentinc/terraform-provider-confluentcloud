e8e294c7c5c83d35d8203ede7896d8213004e2a3
Kostya Linou <klinou@confluent.io>
2021-10-15 19:17:58 +0100

    Remove an example for provider. (#81)

--- examples/provider/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "confluentcloud" {
-  username = var.api_key    # optionally use CONFLUENT_CLOUD_API_KEY env var
-  password = var.api_secret # optionally use CONFLUENT_CLOUD_API_SECRET env var
-
-  # if you want to edit the waiting strategy of terraform apply you can optionally specify the status to wait for
-  # wait_until    = "PROVISIONED"
-}
